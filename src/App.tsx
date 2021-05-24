d59a357e2a15b893f2c4b86f3003e38e7e6363e5
Erik Lundin <erilundin@gmail.com>
2021-05-24 11:01:23 +0200

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./styles/App.module.css";
-import {GroupList} from "components/GroupList";
+import { GroupList } from "components/GroupList";
+import logo from "./assets/uefa-euro-2020.png";
 
 function App() {
     return (
         <div className={styles.app}>
             <header className={styles.header}>
+                <img src={logo} style={{width: "300px"}}/>
                 <h1>EURO 2021</h1>
                 <GroupList />
             </header>
