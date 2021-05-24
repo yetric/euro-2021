66aa87612f4a5a2abe1d6270dc251df1e856977a
Mattias Hising <hising@gmail.com>
2021-05-24 14:16:30 +0200

    WiP Country Lookup (synced with ISO 2 and 3 letters)

---
+++ src/utils/CountryLookup.ts
@@ -0,0 +1,63 @@
+export enum Country {
+    Turkey = "TUR",
+    Italy = "ITA",
+    Wales = "WAL",
+    Switzerland = "CHE",
+    Denmark = "DNK",
+    Finland = "FIN",
+    Belgium = "BEL",
+    Russia = "RUS",
+    Netherlands = "NLD",
+    Ukraine = "UKR",
+    Austria = "AUT",
+    NorthMacedonia = "MKD",
+    England = "ENG",
+    Croatia = "HRV",
+    Scotland = "SCO",
+    Czechia = "CZE",
+    Spain = "ESP",
+    Sweden = "SWE",
+    Poland = "POL",
+    Slovakia = "SVK",
+    Hungary = "HUN",
+    Portugal = "PRT",
+    France = "FRA",
+    Germany = "DEU"
+}
+
+export const CountryAlpha2 = {
+    "TUR": "TR",
+    "ITA": "IT",
+    "WAL": "GB-WLS",
+    "CHE": "CH",
+    "DNK": "DK",
+    "FIN": "FI",
+    "BEL": "BE",
+    "RUS": "RU",
+    "NLD": "NL",
+    "UKR": "UA",
+    "AUT": "AT",
+    "MKD": "MK",
+    "ENG": "GB-ENG",
+    "HRV": "HR",
+    "SCO": "GB-SCT",
+    "CZE": "CZ",
+    "ESP": "ES",
+    "SWE": "SE",
+    "POL": "PL",
+    "SVK": "SK",
+    "HUN": "HU",
+    "PRT": "PT",
+    "FRA": "FR",
+    "DEU": "DE",
+
+}
+
+export const CountryLookup: any = {
+    async getFlag(code: string) {
+        return [];
+    },
+    teamShortToIso(short: string) {
+
+    }
+}
