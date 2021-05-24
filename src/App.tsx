cd3e420232d2e0d02bc8c9071e1c156a7c3ec8f4
Erik Lundin <erilundin@gmail.com>
2021-05-24 10:34:25 +0200

    Added groups json dump

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './styles/App.module.css';
 import {useLoadGroups} from "store/hooks/useLoadGroups";
+import {useSelector} from "react-redux";
+import {groupsStateSelector} from "store/slices/groupsSlice";
 
 function App() {
   useLoadGroups();
+  const groupState = useSelector(groupsStateSelector)
 
   return (
     <div className={styles.app}>
@@ -11,6 +14,7 @@ function App() {
         <h1>EURO 2021</h1>
         <div>
           todo list groups here
+          {JSON.stringify(groupState.groups)}
         </div>
       </header>
     </div>
