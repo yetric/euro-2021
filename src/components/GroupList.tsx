ca48d00d11afc8e3026200e35c8121964fa2ca8b
Erik Lundin <erilundin@gmail.com>
2021-05-24 18:17:01 +0200

    Fixed group list

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -2,22 +2,26 @@ import { useSelector } from "react-redux";
 import { groupsStateSelector } from "store/slices/groupsSlice";
 import { CardGroup } from "react-bootstrap";
 import { GroupCard } from "components/GroupCard";
-import { Group } from "../store/models";
-import { chunks } from "../utils/core";
+import { GroupDetailed } from "store/models";
+import { chunks } from "utils/core";
 import styles from "./styles/GroupList.module.css";
+import { teamsStateSelector } from "store/slices/teamsSlice";
+import { getAllGroupsSelector } from "store/selectors";
 
 export const GroupList = (): JSX.Element => {
     const groupState = useSelector(groupsStateSelector);
+    const teamsState = useSelector(teamsStateSelector);
+    const groupsWithTeams = useSelector(getAllGroupsSelector);
 
-    if (groupState.isLoading) return <div>Loading...</div>;
+    if (groupState.isLoading || teamsState.isLoading) return <div>Loading...</div>;
 
     return (
         <>
-            {chunks(groupState.groups, 3).map((chunk, index) => {
+            {chunks(groupsWithTeams, 3).map((chunk, index) => {
                 return (
                     <CardGroup key={"row-" + index} className={styles.group}>
-                        {chunk.map((group: Group, index: number) => {
-                            return <GroupCard key={"card-"+index} group={group} />;
+                        {chunk.map((group: GroupDetailed, index: number) => {
+                            return <GroupCard key={"card-" + index} group={group} />;
                         })}
                     </CardGroup>
                 );
