7ee02d90480832cfc1fcc9feb4821aec31c9f545
Mattias Hising <hising@gmail.com>
2021-05-29 21:35:35 +0200

    WiP - rand bets (for dev purpose)

--- src/views/TipsView.tsx
+++ src/views/TipsView.tsx
@@ -3,13 +3,15 @@ import { getAllMatches, getUserBets } from "../store/slices/matchesSlice";
 import { BetOnGame } from "../components/BetOnGame";
 import { useEffect, useState } from "react";
 import { Match } from "../store/models";
-import { ProgressBar } from "react-bootstrap";
+import { Button, ProgressBar } from "react-bootstrap";
 
 interface BettingProps {
     home: number;
     away: number;
     gameId: string;
 }
+
+const randomInt = (min: number, max: number) =>  Math.floor(Math.random() * (max - min + 1) + min)
 
 export const TipsView = () => {
     const games = useSelector(getAllMatches());
@@ -27,13 +29,29 @@ export const TipsView = () => {
     const percentageDone = (Object.entries(betting).length / games.length) * 100;
     const percentageDoneRounded = Math.round(percentageDone * 10) / 10;
 
+    const randomize = () => {
+        games.forEach((game: Match) => {
+            const home = randomInt(0,3);
+            const away = randomInt(0,3);
+            bets[game.id] = [home, away]
+        });
+    };
+
     return (
         <div>
             <h1 className={"h3"}>Bet on Results in Euro 2020</h1>
 
+
             {!isNaN(percentageDone) && <ProgressBar style={{
                 height: "32px"
             }} now={percentageDone} label={`${percentageDoneRounded}%  `} />}
+
+            <p className={"mt-3"}>
+                <Button size={"sm"} variant={"outline-danger"} onClick={(event) => {
+                    event.preventDefault();
+                    randomize();
+            }}>Randomize</Button>
+            </p>
 
             {games.map((game: Match, index: number) => {
                 return (
